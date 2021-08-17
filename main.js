4b47574f302c1ec30433879e696e5af6bf703a96
code express <codeexpress333@gmail.com>
2021-08-17 11:05:43 +0530

    parallax

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    $('.angle').click(function (e) {
+        $('.angle').toggleClass('arrow');
+    });
+
+    $('.angle').first().addClass('first');
+    $('.angle').last().addClass('last');
+
+});
+
